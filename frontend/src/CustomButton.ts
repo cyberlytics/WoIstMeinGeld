a09f51da0cab05e84e4331d8fa3217db7f495802
Ulrich Stark <u.stark@oth-aw.de>
2022-05-25 21:45:00 +0200

    simplify themed components

--- frontend/src/CustomButton.ts
+++
@@ -1,9 +0,0 @@
-import { styled, Button } from "@mui/material";
-
-const CustomButton = styled(Button)(({ theme, color = "primary" }) => ({
-    ":hover": {
-        backgroundColor: theme.palette[color].dark,
-    },
-}));
-
-export default CustomButton;

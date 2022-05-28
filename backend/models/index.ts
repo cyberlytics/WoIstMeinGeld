7783bfecbff3bd6cf4b94951b8424fe35ab85379
Leo Schurrer <l.schurrer@oth-aw.de>
2022-05-28 23:43:37 +0200

    delete friendship example code

--- backend/models/index.ts
+++ backend/models/index.ts
@@ -1,7 +1,6 @@
 import { Sequelize } from "sequelize";
 import dbCfg from "../config/db.config";
 import person from "./person.model";
-import friendship from "./friendship.model";
 import transaction from "./transaction.model";
 
 const seq = new Sequelize(dbCfg.DB, dbCfg.USER, dbCfg.PASSWORD, {
@@ -15,7 +14,6 @@ const db = {
     sequelize: seq,
     persons: person(seq),
     transactions: transaction(seq),
-    friendships: friendship(seq),
 };
 
 export default db;

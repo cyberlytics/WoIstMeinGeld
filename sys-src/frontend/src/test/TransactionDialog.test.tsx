76f2a64d03f5f64b6906e32fe15670f4cb3d24ec
Ronja Bäumel <r.baeumel@oth-aw.de>
2022-06-05 22:43:24 +0200

    update input-validation and UI-tests

--- sys-src/frontend/src/test/TransactionDialog.test.tsx
+++ sys-src/frontend/src/test/TransactionDialog.test.tsx
@@ -16,15 +16,77 @@ describe("TransactionDialog", () => {
         const button = getByLabelText(container, "openDialogButton");
         fireEvent.click(button);
 
+        //check if dialog pops up
+        expect(screen.getByRole("dialog").getAttribute("open")).toBeTruthy;
+
         // find the Home MenuItem
         const menuItem = screen.getByRole("dialog");
 
         // make sure it was rendered
+
         expect(await getByText(menuItem, "Verwendungszweck")).toBeTruthy();
         expect(await getByText(menuItem, "Gläubiger")).toBeTruthy();
         expect(await getByText(menuItem, "Betrag")).toBeTruthy();
         expect(await getByText(menuItem, "Zahlungszeitpunkt")).toBeTruthy();
         expect(await getByText(menuItem, "Anlegen")).toBeTruthy();
         expect(await getByText(menuItem, "Abbrechen")).toBeTruthy();
+    });
+
+    test("buttons are clickable", () => {
+        const { container } = render(
+            <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={deLocale}>
+                <TransactionDialog />
+            </LocalizationProvider>
+        );
+        const button = getByLabelText(container, "openDialogButton");
+        fireEvent.click(button);
+
+        // find the Home MenuItem
+        const menuItem = screen.getByRole("dialog");
+
+        expect(getByText(menuItem, "Anlegen")).toBeEnabled();
+        expect(getByText(menuItem, "Abbrechen")).toBeEnabled();
+    });
+
+    test("if dialog closes on pressed Close button", async () => {
+        const { container } = render(
+            <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={deLocale}>
+                <TransactionDialog />
+            </LocalizationProvider>
+        );
+
+        const button = getByLabelText(container, "openDialogButton");
+        fireEvent.click(button);
+
+        // find the Home MenuItem
+        const menuItem = screen.getByRole("dialog");
+
+        //close dialog
+        const buttonEscape = getByText(menuItem, "Abbrechen");
+        fireEvent.click(buttonEscape);
+
+        expect(screen.getByRole("dialog").getAttribute("open")).toBeFalsy;
+    });
+
+    test("if dialog shows an error when as user-iput is missing", async () => {
+        const { container } = render(
+            <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={deLocale}>
+                <TransactionDialog />
+            </LocalizationProvider>
+        );
+
+        const button = getByLabelText(container, "openDialogButton");
+        fireEvent.click(button);
+
+        // find the Home MenuItem
+        const menuItem = screen.getByRole("dialog");
+
+        const buttonCreate = getByText(menuItem, "Anlegen");
+        fireEvent.click(buttonCreate);
+
+        const descriptionInput = getByText(menuItem, "Verwendungszweck");
+
+        // check if error
+        expect(await getByText(menuItem, "Verwendungszweck").getAttribute("error")).toBeTruthy();
     });
 });

3456ef6d1ebbffa0af1cabc49963cf1c522dc760
Alexander Ziebell <a.ziebell@oth-aw.de>
2022-06-08 18:07:45 +0200

    - added initial version of algorithm for calculating repayments
    - modified test data

--- sys-src/frontend/src/components/RepaymentList.tsx
+++ sys-src/frontend/src/components/RepaymentList.tsx
@@ -3,6 +3,7 @@ import { useMemo } from "react";
 import { Group } from "../models/Group";
 import { Repayment } from "../models/Repayment";
 import { Transaction } from "../models/Transaction";
+import { repay_algo } from "../repayment_algorithm";
 import { RepaymentListItem } from "./RepaymentListItem";
 
 interface Props {
@@ -15,17 +16,7 @@ export function RepaymentList(props: Props) {
     const { group, transactions, onReload } = props;
 
     const repayments: Repayment[] = useMemo(() => {
-        // demo algorithm to generate repayments
-
-        if (transactions.length >= 3) {
-            return [
-                { from: transactions[0].creditor, to: transactions[2].creditor, amount: 13.37 },
-                { from: transactions[1].creditor, to: transactions[0].creditor, amount: 2 },
-                { from: transactions[2].creditor, to: transactions[1].creditor, amount: 25 },
-            ];
-        }
-
-        return [];
+        return repay_algo(transactions);
     }, []);
 
     return repayments.length ? (

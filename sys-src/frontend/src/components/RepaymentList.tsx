8b2ef9fa4bdc13e54bc2634d4e606948089d7769
Leo Schurrer <l.schurrer@oth-aw.de>
2022-06-16 15:39:06 +0200

    inital diagram

--- sys-src/frontend/src/components/RepaymentList.tsx
+++ sys-src/frontend/src/components/RepaymentList.tsx
@@ -5,6 +5,7 @@ import { Repayment } from "../models/Repayment";
 import { Transaction } from "../models/Transaction";
 import { calculateRepayments } from "../calculateRepayments";
 import { RepaymentListItem } from "./RepaymentListItem";
+import { PieChart, Pie, Legend, ResponsiveContainer } from "recharts";
 
 interface Props {
     groupId: number;
@@ -20,16 +21,28 @@ export function RepaymentList(props: Props) {
     }, []);
 
     return repayments.length ? (
-        <List>
-            {repayments.map((repayment) => (
-                <RepaymentListItem
-                    key={repayment.from.id + "" + repayment.to.id}
-                    groupId={groupId}
-                    repayment={repayment}
-                    onReload={onReload}
+        <>
+            <List>
+                {repayments.map((repayment) => (
+                    <RepaymentListItem
+                        key={repayment.from.id + "" + repayment.to.id}
+                        groupId={groupId}
+                        repayment={repayment}
+                        onReload={onReload}
+                    />
+                ))}
+            </List>
+            <PieChart width={300} height={300}>
+                <Pie
+                    data={repayments}
+                    nameKey={(repayment: Repayment) => repayment.from.name}
+                    dataKey={"amount"}
+                    innerRadius={60}
+                    outerRadius={100}
                 />
-            ))}
-        </List>
+                <Legend />
+            </PieChart>
+        </>
     ) : (
         <Typography variant="h5" align="center" style={{ padding: 20 }}>
             Keine Rückzahlungen nötig

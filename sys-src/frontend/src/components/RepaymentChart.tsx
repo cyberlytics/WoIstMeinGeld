20ae6bacf25f3fb262ed6362e58e3993f4d14897
Leo Schurrer <l.schurrer@oth-aw.de>
2022-06-19 17:17:26 +0200

    add barchart

--- sys-src/frontend/src/components/RepaymentChart.tsx
+++ sys-src/frontend/src/components/RepaymentChart.tsx
@@ -1,8 +1,12 @@
 import { useTheme } from "@mui/material";
-import { useState } from "react";
-import { Cell, ResponsiveContainer } from "recharts";
+import { Bar, BarChart, Cell, LabelList, ResponsiveContainer, Sector, XAxis, YAxis } from "recharts";
 import { formatMoney } from "../format/formatMoney";
 import { Repayment } from "../models/Repayment";
+
+interface Total {
+    name: string;
+    value: number;
+}
 
 interface Props {
     repayments: Repayment[];
@@ -16,5 +20,70 @@ export function RepaymentChart(props: Props) {
         typography: { fontFamily, fontSize },
     } = useTheme();
 
-    return <div id={"repayment-chart"}></div>;
+    const barData = repayments.reduce((totals: Total[], repayment): Total[] => {
+        // from debtor
+        let temp = totals.find((total) => total.name == repayment.from.name);
+        if (temp) temp.value -= repayment.amount;
+        else
+            totals.push({
+                name: repayment.from.name,
+                value: repayment.amount * -1,
+            });
+        // to creditor
+        temp = totals.find((total) => total.name == repayment.to.name);
+        if (temp) temp.value += repayment.amount;
+        else
+            totals.push({
+                name: repayment.to.name,
+                value: repayment.amount,
+            });
+        return totals;
+    }, []);
+
+    return (
+        <div id={"repayment-chart"}>
+            <ResponsiveContainer width={"60%"}>
+                <BarChart
+                    data={barData.map((v) =>
+                        Object.assign({}, v, { label: { value: v.value, palette, fontFamily, fontSize } })
+                    )}
+                    margin={{ top: 30, left: 5, bottom: 30, right: 5 }}
+                >
+                    <Bar dataKey={"value"}>
+                        {barData.map((v, i) => (
+                            <Cell
+                                key={`slice-${i}`}
+                                fill={v.value < 0 ? palette.badRed.main : palette.goodGreen.main}
+                            />
+                        ))}
+                        <LabelList dataKey={"label"} content={renderCustomizedLabel} />
+                    </Bar>
+                    <XAxis dataKey={"name"} stroke={palette.notFilledGray.main} />
+                    <YAxis stroke={palette.notFilledGray.main} />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
+    );
 }
+
+const renderCustomizedLabel = (props: any) => {
+    const { x, y, width, height, value: payload, viewBox } = props;
+
+    console.log(height);
+    const baseLine = height < 0 && Math.abs(height) < payload.fontSize ? "hanging" : "top";
+
+    return (
+        <g>
+            <text
+                x={x + width / 2}
+                y={y}
+                fontFamily={payload.fontFamily}
+                fill={payload.palette.fontWhite.main}
+                textAnchor="middle"
+                dominantBaseline={baseLine}
+            >
+                {formatMoney(payload.value)}
+            </text>
+        </g>
+    );
+};

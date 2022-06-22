85503d31eccf83c37d30ef1844da518405d5d265
Alexander Ziebell <a.ziebell@oth-aw.de>
2022-06-22 12:43:25 +0200

    tests for repayment calculation refactored

--- sys-src/frontend/src/test/calculateRepayments.test.ts
+++ sys-src/frontend/src/test/calculateRepayments.test.ts
@@ -3,23 +3,46 @@ import { Person } from "../models/Person";
 import { Repayment } from "../models/Repayment";
 import { Transaction } from "../models/Transaction";
 
-const people: Person[] = [
-    { id: 1, name: "Alice" },
-    { id: 2, name: "Bob" },
-    { id: 3, name: "Charlie" },
-    { id: 4, name: "Dave" },
-];
-
-const aliceTransaction: Transaction = {
+const alice: Person = {
     id: 1,
-    group_id: 1,
-    description: "description",
-    creditor_id: 1, // Alice
-    amount: 0,
-    time: "2022-06-15 12:00:00",
-    creditor: people[0], // Alice
-    debtors: [], // nobody
+    name: "Alice",
 };
+
+const bob: Person = {
+    id: 2,
+    name: "Bob",
+};
+
+const charlie: Person = {
+    id: 3,
+    name: "Charlie",
+};
+
+const dave: Person = {
+    id: 4,
+    name: "Dave",
+};
+
+function createTransaction(
+    id: number,
+    group_id: number,
+    description: string,
+    creditor_id: number,
+    amount: number,
+    creditor: Person,
+    debtors: Person[]
+): Transaction {
+    return {
+        id: id,
+        group_id: group_id,
+        description: description,
+        creditor_id: creditor_id,
+        amount: amount,
+        time: "2022-06-01 12:00:00",
+        creditor: creditor,
+        debtors: debtors,
+    };
+}
 
 describe("calculateRepayments", () => {
     test("[1] no transactions", () => {
@@ -29,56 +52,47 @@ describe("calculateRepayments", () => {
 
         expect(repayments).toHaveLength(0);
     });
+
     test("[2] one transaction without debtors", () => {
-        let oneTransaction: Transaction[] = [
-            {
-                ...aliceTransaction,
-                description: "Alice lost a five euro bill.",
-                amount: 5,
-            },
-        ];
+        let aliceTransaction = createTransaction(1, 1, "Alice lost a five euro bill.", 1, 5, alice, []);
+
+        let oneTransaction: Transaction[] = [aliceTransaction];
 
         let repayments: Repayment[] = calculateRepayments(oneTransaction);
 
         expect(repayments).toHaveLength(0);
     });
+
     test("[3] one transaction where the creditor is the only debtor", () => {
-        let oneTransaction: Transaction[] = [
-            {
-                ...aliceTransaction,
-                description: "Alice bought something just for her.",
-                amount: 5,
-                debtors: people.slice(0, 1), // Alice
-            },
-        ];
+        let aliceTransaction = createTransaction(1, 1, "Alice bought something just for her.", 1, 5, alice, [alice]);
+
+        let oneTransaction: Transaction[] = [aliceTransaction];
 
         let repayments: Repayment[] = calculateRepayments(oneTransaction);
 
         expect(repayments).toHaveLength(0);
     });
+
     test("[4] one small transaction (0.03€)", () => {
-        let oneTransaction: Transaction[] = [
-            {
-                ...aliceTransaction,
-                description: "Alice lent three cent to Bob and Charlie.",
-                amount: 0.03,
-                debtors: people.slice(1, 3), // Bob, Charlie
-            },
-        ];
+        let aliceTransaction = createTransaction(1, 1, "Alice lent three cent to Bob and Charlie.", 1, 0.03, alice, [
+            bob,
+            charlie,
+        ]);
+
+        let oneTransaction: Transaction[] = [aliceTransaction];
 
         let repayments: Repayment[] = calculateRepayments(oneTransaction);
 
         expect(repayments).toHaveLength(0);
     });
+
     test("[5] one small transaction (0.04€)", () => {
-        let oneTransaction: Transaction[] = [
-            {
-                ...aliceTransaction,
-                description: "Alice lent four cent to Bob and Charlie.",
-                amount: 0.04,
-                debtors: people.slice(1, 3), // Bob, Charlie
-            },
-        ];
+        let aliceTransaction = createTransaction(1, 1, "Alice lent four cent to Bob and Charlie.", 1, 0.04, alice, [
+            bob,
+            charlie,
+        ]);
+
+        let oneTransaction: Transaction[] = [aliceTransaction];
 
         let repayments: Repayment[] = calculateRepayments(oneTransaction);
 
@@ -90,15 +104,14 @@ describe("calculateRepayments", () => {
         expect(repaymentToIds).toEqual([1, 1]);
         expect(repaymentAmounts).toEqual([0.02, 0.02]);
     });
+
     test("[6] one small transaction (0.05€)", () => {
-        let oneTransaction: Transaction[] = [
-            {
-                ...aliceTransaction,
-                description: "Alice lent five cent to Bob and Charlie.",
-                amount: 0.05,
-                debtors: people.slice(1, 3), // Bob, Charlie
-            },
-        ];
+        let aliceTransaction = createTransaction(1, 1, "Alice lent five cent to Bob and Charlie.", 1, 0.05, alice, [
+            bob,
+            charlie,
+        ]);
+
+        let oneTransaction: Transaction[] = [aliceTransaction];
 
         let repayments: Repayment[] = calculateRepayments(oneTransaction);
 
@@ -110,15 +123,15 @@ describe("calculateRepayments", () => {
         expect(repaymentToIds).toEqual([1, 1]);
         expect(repaymentAmounts).toEqual([0.03, 0.02]);
     });
+
     test("[7] one normal transaction (1€)", () => {
-        let oneTransaction: Transaction[] = [
-            {
-                ...aliceTransaction,
-                description: "Alice lent one euro to Bob, Charlie and Dave.",
-                amount: 1,
-                debtors: people.slice(1), // Bob, Charlie, Dave
-            },
-        ];
+        let aliceTransaction = createTransaction(1, 1, "Alice lent one euro to Bob, Charlie and Dave.", 1, 1, alice, [
+            bob,
+            charlie,
+            dave,
+        ]);
+
+        let oneTransaction: Transaction[] = [aliceTransaction];
 
         let repayments: Repayment[] = calculateRepayments(oneTransaction);
 
@@ -130,15 +143,16 @@ describe("calculateRepayments", () => {
         expect(repaymentToIds).toEqual([1, 1, 1]);
         expect(repaymentAmounts).toEqual([0.33, 0.33, 0.34]);
     });
+
     test("[8] one normal transaction (1€)", () => {
-        let oneTransaction: Transaction[] = [
-            {
-                ...aliceTransaction,
-                description: "Alice spent one euro for everybody.",
-                amount: 1,
-                debtors: people, // Alice, Bob, Charlie, Dave
-            },
-        ];
+        let aliceTransaction = createTransaction(1, 1, "Alice spent one euro for everybody.", 1, 1, alice, [
+            alice,
+            bob,
+            charlie,
+            dave,
+        ]);
+
+        let oneTransaction: Transaction[] = [aliceTransaction];
 
         let repayments: Repayment[] = calculateRepayments(oneTransaction);
 
@@ -150,15 +164,15 @@ describe("calculateRepayments", () => {
         expect(repaymentToIds).toEqual([1, 1, 1]);
         expect(repaymentAmounts).toEqual([0.25, 0.25, 0.25]);
     });
+
     test("[9] one normal transaction (2€)", () => {
-        let oneTransaction: Transaction[] = [
-            {
-                ...aliceTransaction,
-                description: "Alice lent two euro to Bob, Charlie and Dave.",
-                amount: 2,
-                debtors: people.slice(1), // Bob, Charlie, Dave
-            },
-        ];
+        let aliceTransaction = createTransaction(1, 1, "Alice lent two euro to Bob, Charlie and Dave.", 1, 2, alice, [
+            bob,
+            charlie,
+            dave,
+        ]);
+
+        let oneTransaction: Transaction[] = [aliceTransaction];
 
         let repayments: Repayment[] = calculateRepayments(oneTransaction);
 
@@ -170,15 +184,16 @@ describe("calculateRepayments", () => {
         expect(repaymentToIds).toEqual([1, 1, 1]);
         expect(repaymentAmounts).toEqual([0.67, 0.67, 0.66]);
     });
+
     test("[10] one normal transaction (2€)", () => {
-        let oneTransaction: Transaction[] = [
-            {
-                ...aliceTransaction,
-                description: "Alice spent two euro for everybody.",
-                amount: 2,
-                debtors: people, // Alice, Bob, Charlie, Dave
-            },
-        ];
+        let aliceTransaction = createTransaction(1, 1, "Alice spent two euro for everybody.", 1, 2, alice, [
+            alice,
+            bob,
+            charlie,
+            dave,
+        ]);
+
+        let oneTransaction: Transaction[] = [aliceTransaction];
 
         let repayments: Repayment[] = calculateRepayments(oneTransaction);
 
@@ -190,49 +205,37 @@ describe("calculateRepayments", () => {
         expect(repaymentToIds).toEqual([1, 1, 1]);
         expect(repaymentAmounts).toEqual([0.5, 0.5, 0.5]);
     });
+
     test("[11] four normal transactions (1€, 2€, 3€, 4€)", () => {
-        let fourTransactions: Transaction[] = [
-            {
-                id: 1,
-                group_id: 1,
-                description: "Alice spent one euro for everybody.",
-                creditor_id: 1, // Alice
-                amount: 1,
-                time: "2022-06-01 12:00:00",
-                creditor: people[0], // Alice
-                debtors: people, // Alice, Bob, Charlie, Dave
-            },
-            {
-                id: 2,
-                group_id: 1,
-                description: "Bob spent two euro for everybody.",
-                creditor_id: 2, // Bob
-                amount: 2,
-                time: "2022-06-02 12:00:00",
-                creditor: people[1], // Bob
-                debtors: people, // Alice, Bob, Charlie, Dave
-            },
-            {
-                id: 3,
-                group_id: 1,
-                description: "Charlie spent three euro for everybody.",
-                creditor_id: 3, // Charlie
-                amount: 3,
-                time: "2022-06-03 12:00:00",
-                creditor: people[2], // Charlie
-                debtors: people, // Alice, Bob, Charlie, Dave
-            },
-            {
-                id: 4,
-                group_id: 1,
-                description: "Dave spent four euro for everybody.",
-                creditor_id: 4, // Dave
-                amount: 4,
-                time: "2022-06-03 12:00:00",
-                creditor: people[3], // Dave
-                debtors: people, // Alice, Bob, Charlie, Dave
-            },
-        ];
+        let aliceTransaction = createTransaction(1, 1, "Alice spent one euro for everybody.", 1, 1, alice, [
+            alice,
+            bob,
+            charlie,
+            dave,
+        ]);
+
+        let bobTransaction = createTransaction(2, 1, "Bob spent two euro for everybody.", 2, 2, bob, [
+            alice,
+            bob,
+            charlie,
+            dave,
+        ]);
+
+        let charlieTransaction = createTransaction(3, 1, "Charlie spent three euro for everybody.", 3, 3, charlie, [
+            alice,
+            bob,
+            charlie,
+            dave,
+        ]);
+
+        let daveTransaction = createTransaction(4, 1, "Dave spent four euro for everybody.", 4, 4, dave, [
+            alice,
+            bob,
+            charlie,
+            dave,
+        ]);
+
+        let fourTransactions: Transaction[] = [aliceTransaction, bobTransaction, charlieTransaction, daveTransaction];
 
         let repayments: Repayment[] = calculateRepayments(fourTransactions);
 
@@ -244,49 +247,14 @@ describe("calculateRepayments", () => {
         expect(repaymentToIds).toEqual([4, 3]);
         expect(repaymentAmounts).toEqual([1.5, 0.5]);
     });
+
     test("[12] four small transactions (0.01€, 0.01€, 0.01€, 0.01€)", () => {
-        let fourTransactions: Transaction[] = [
-            {
-                id: 1,
-                group_id: 1,
-                description: "Alice lent one cent to Bob and Charlie.",
-                creditor_id: 1, // Alice
-                amount: 0.01,
-                time: "2022-06-01 12:00:00",
-                creditor: people[0], // Alice
-                debtors: people.slice(1, 3), // Bob, Charlie
-            },
-            {
-                id: 2,
-                group_id: 1,
-                description: "Alice lent one cent to Bob and Charlie.",
-                creditor_id: 1, // Alice
-                amount: 0.01,
-                time: "2022-06-01 12:00:00",
-                creditor: people[0], // Alice
-                debtors: people.slice(1, 3), // Bob, Charlie
-            },
-            {
-                id: 3,
-                group_id: 1,
-                description: "Alice lent one cent to Bob and Charlie.",
-                creditor_id: 1, // Alice
-                amount: 0.01,
-                time: "2022-06-01 12:00:00",
-                creditor: people[0], // Alice
-                debtors: people.slice(1, 3), // Bob, Charlie
-            },
-            {
-                id: 4,
-                group_id: 1,
-                description: "Alice lent one cent to Bob and Charlie.",
-                creditor_id: 1, // Alice
-                amount: 0.01,
-                time: "2022-06-01 12:00:00",
-                creditor: people[0], // Alice
-                debtors: people.slice(1, 3), // Bob, Charlie
-            },
-        ];
+        let aliceTransaction = createTransaction(1, 1, "Alice lent one cent to Bob and Charlie.", 1, 0.01, alice, [
+            bob,
+            charlie,
+        ]);
+
+        let fourTransactions: Transaction[] = [aliceTransaction, aliceTransaction, aliceTransaction, aliceTransaction];
 
         let repayments: Repayment[] = calculateRepayments(fourTransactions);
 
